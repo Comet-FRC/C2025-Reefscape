85360b349710e0975da851a561f3d057fc98954d
Jacob Lee <jelee231@gmail.com>
2025-02-15 15:27:22 -0500

    Add documentation to AllianceColor utility class for clarity

--- src/main/java/frc/robot/util/AllianceColor.java
+++ src/main/java/frc/robot/util/AllianceColor.java
@@ -5,7 +5,14 @@ import java.util.Optional;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 
+/**
+ * A utility class for determining the alliance color of the robot.
+ */
 public final class AllianceColor {
+
+    /**
+     * @return true if the robot is on the red alliance
+     */
     public static final boolean isRed() {
         Optional<Alliance> ally = DriverStation.getAlliance();
         return ally.isPresent() && ally.get() == Alliance.Red;
