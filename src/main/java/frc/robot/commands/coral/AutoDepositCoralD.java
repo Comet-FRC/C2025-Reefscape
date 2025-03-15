0ed253e76c70a74056fc6aae2ea365e6c9011a58
Jacob Lee <jelee231@gmail.com>
2025-03-15 17:07:35 -0400

    Update at 'Sat Mar 15 17:07:35 EDT 2025'

--- src/main/java/frc/robot/commands/coral/AutoDepositCoralD.java
+++ src/main/java/frc/robot/commands/coral/AutoDepositCoralD.java
@@ -19,7 +19,7 @@ public class AutoDepositCoralD extends SequentialCommandGroup {
 
         super(
             drive.moveToPosePID(() -> {
-                    Pose2d pose = new Pose2d(new Translation2d(6.144, 3.738), Rotation2d.fromDegrees(90));
+                    Pose2d pose = new Pose2d(new Translation2d(6.2, 3.738), Rotation2d.fromDegrees(90));
                     return FieldMirroringUtils.toCurrentAlliancePose(pose);
                 }
             ),
